7cee2b38fc6c75490325490171d1179c862e2495
Paul Grochocinski <grochocinski-paul@grochoci.private>
2013-11-02 00:17:22 -0400

    Problem_3

---
+++ Problem_1.py
@@ -0,0 +1,20 @@
+def prob_1():
+    """ Sum of all Natural numbers below 1000 that are
+    multiples of 3 and 5.
+    """
+    
+    num_sum = 0
+    for num in range(1000):
+        if (num % 3 == 0) or (num % 5 == 0):
+<<<<<<< HEAD
+<<<<<<< HEAD
+            num_sum += num
+    return num_sum
+=======
+            sum += num
+    return sum
+>>>>>>> f31d51ba149d467fdf7acf60a34af92fad092e04
+=======
+            num_sum += num
+    return num_sum
+>>>>>>> 366857921dda4c26c1528325be99dad2949decbb
